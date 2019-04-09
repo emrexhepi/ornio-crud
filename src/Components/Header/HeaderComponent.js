c9fe0ab9f2c3bd23ced9e9d5d6e33426e79c9b88
Emrullah Rexhepi <em.rexhepi@gmail.com>
2019-04-09 20:59:14 +0200

    small design bugs

--- src/Components/Header/HeaderComponent.js
+++ src/Components/Header/HeaderComponent.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 import {
   Navbar,
-  Container
+  Container,
+  Col,
+  Row,
 } from 'react-bootstrap';
 
 // import styles
@@ -11,16 +15,26 @@ import styles from './HeaderComponent.module.scss';
 class HeaderComponent extends Component {
   render() {
     return (
-      <Navbar bg="light" fixed="top" variant="light" className={`${styles.HeaderComponent} navbar-static-top`}>
+      <Navbar bg="light" fixed="top" variant="light" className={`${styles.HeaderComponent} navbar-static-top justify-content-between`}>
         <Container>
-          <Navbar.Brand className={styles.Brand}>
-            <span>PWD</span>
-            <span>RESET</span>
-          </Navbar.Brand>
+            <Col xs sm md={8} className={styles.NavbarContainer}>
+              <Navbar.Brand className={styles.Brand}>
+                <span>PWD</span>
+                <span>RESET</span>
+              </Navbar.Brand>
+            </Col>
+
+            <Col xs sm md={4} className={styles.Name}>
+              <span> {this.props.user && this.props.user.name }</span>
+            </Col>
         </Container>
       </Navbar>
     )
   }
 }
 
-export default HeaderComponent;
+const mapStateToProps = (state) => ({
+  user: state.user && state.user.details
+})
+
+export default connect(mapStateToProps)(HeaderComponent);
