74a1883d67aa34ac7f5d1a420859ef35a1c55f94
Emrullah Rexhepi <em.rexhepi@gmail.com>
2019-04-09 14:18:55 +0200

    base axios setup with default bootstrap

---
+++ axios.js
@@ -0,0 +1,7 @@
+import Axios from "axios";
+
+export const setupAxios = (store) => {
+
+}
+
+export default Axios;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import axios custom config function
+import { setupAxios } from './axios';
+
 // redux store
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 
+// create store
+const store = configureStore();
+
+// configure axios with store
+setupAxios(store);
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
+  <Provider store={store}>
     <App />
   </Provider>
 , document.getElementById('root'));
